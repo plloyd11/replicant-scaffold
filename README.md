f2a99df91abb8321226e167a60a7c8804a33014a
Pete Lloyd <plloyd11@gmail.com>
2020-07-30 18:09:36 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Eleventy + Tailwind + AlpineJS Starter
+
+SSG Starter using Eleventy, Tailwind & AlpineJS.
+
+## Starting The Dev Server
+
+``` bash
+yarn start
+```
+
+## Building For Production
+
+``` bash
+yarn build
+```
---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: [
+    require(`tailwindcss`)(`./styles/tailwind.config.js`),
+    require(`autoprefixer`),
+    ...(process.env.NODE_ENV === 'production' ? [require(`postcss-clean`)] : []),
+  ],
+}
